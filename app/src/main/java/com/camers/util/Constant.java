51a41d99a3ba1ad3c0e1144bca694156b5ffcd80
yangshiyun <1623537198@qq.com>
2021-06-11 10:04:38 +0800

    第一次提交

--- app/src/main/java/com/camers/util/Constant.java
+++ app/src/main/java/com/camers/util/Constant.java
@@ -2,7 +2,7 @@ package com.camers.util;
 
 public class Constant {
     public static String BASEURL="http://218.62.228.154:8989/camera/";
-    //public static String BASEURL="http://192.168.59.1:8081/";
+    //public static String BASEURL="http://192.168.110.65:8989/";
     public static double LONGITUDE = 0;//经度
     public static double LATITUDE = 0;//纬度
 }
